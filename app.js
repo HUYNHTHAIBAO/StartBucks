ad743ea2ced188c4186664189c7affc4c623576e
Huỳnh Thái Bảo <huynhthaibao.it@gmail.com>
2022-08-22 19:20:06 +0700

    new project

---
+++ app.js
@@ -0,0 +1,7 @@
+function imgSlider(img) {
+    document.querySelector('.starbucks').src = img;
+}
+function changeColor(color) {
+   const cricle = document.querySelector('.cricle');
+    cricle.style.background = color;
+}
